13ec3d41266175f831898b2d237a0f57e090dc3f
xtj123 <13907783+xtj123@user.noreply.gitee.com>
2024-05-16 11:51:21 +0800

    16

--- debounce.js
+++ debounce.js
@@ -1,15 +1,13 @@
 function debounce(fn, delay) {
-  let timer = null,
-    context = this,
-    args = arguments;
+  let timer ;
   return function () {
-    if (timer) {
+    let args = [...arguments];
+    if (timer) {//清除上一次的定时器
       clearTimeout(timer);
-      timer = null;
     }
     timer = setTimeout(() => {
-      fn.apply(context, args);
-    }, delay);
+      fn.apply(this, args);//this显式绑定
+    }, delay);//定时器会修改this指向 定时器词法环境大多数都是window
   };
 }
 function iDebounce(func, delay, immediate) {
